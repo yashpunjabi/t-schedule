9a5eaaa8867b5f2d8d261513daf8522c0140f4b4
Yash Punjabi <yashrpunjabi@gmail.com>
2016-10-05 17:28:27 -0400

    Login/Signup page

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,102 @@
+var app = angular.module('tschedule', ['ui.router', 'firebase'])
+
+app.controller('HomeCtrl', [
+    '$scope',
+    '$firebaseAuth',
+    function($scope, $firebaseAuth) {
+
+        var auth = $firebaseAuth();
+
+        $scope.signin = function() {
+
+            if ($scope.email == '' || $scope.password == '') {
+                return;
+            }
+
+            $scope.firebaseUser = null;
+
+            auth.$signInWithEmailAndPassword($scope.email, $scope.password)
+                .then(function(user) {
+                    $scope.firebaseUser = user;
+                    alert("Sucessfully logged in!");
+                })
+                .catch(function(error) {
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  alert("" + errorCode + ": " + errorMessage);
+                });
+            $scope.email = '';
+            $scope.password = '';
+        };
+
+        $scope.signup = function() {
+
+            if ($scope.fname == '' || $scope.lname == '' || $scope.email == '' || $scope.password == '' || $scope.password2 == '') {
+                return;
+            }
+
+            if ($scope.password != $scope.password2) {
+                alert("Passwords don't match");
+                $scope.password = '';
+                $scope.password2 = '';
+                return;
+            }
+
+            $scope.firebaseUser = null;
+
+            auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+                .then(function(user) {
+                    $scope.firebaseUser = user;
+                    alert("Sucessfully created account!");
+                })
+                .catch(function(error) {
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  alert("" + errorCode + ": " + errorMessage);
+                });
+            $scope.fname = '';
+            $scope.lname = '';
+            $scope.email = '';
+            $scope.password = '';
+            $scope.password2 = '';
+        };
+
+        //TODO
+        $scope.signinFacebook = function() {
+            var provider = new firebase.auth.FacebookAuthProvider();
+            auth.$signInWithPopup(provider).then(function(result) {
+                var token = result.credential.accessToken;
+                $scope.firebaseUser = result.user;
+                alert("login successful, " + $scope.firebaseUser.displayName);
+            }).catch(function(error) {
+                alert("" + error.code + ": " + error.message);
+            });
+        }
+
+        //TODO
+        $scope.forgotPassword = function() {
+            alert("Coming soon");
+        }
+    }
+]);
+
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('signin', {
+                url: '/signin',
+                templateUrl: '/signin.ejs',
+                controller: 'HomeCtrl',
+            });
+        $stateProvider
+            .state('signup', {
+                url: '/signup',
+                templateUrl: '/signup.ejs',
+                controller: 'HomeCtrl',
+            });
+        $urlRouterProvider.otherwise('signin');
+    }
+])

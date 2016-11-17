e7b2707055d570f8e61db318f1e123c0a284b01a
Yash Punjabi <yashrpunjabi@gmail.com>
2016-11-16 20:50:39 -0500

    Start forum stuff

---
+++ public/javascripts/angularForum.js
@@ -0,0 +1,35 @@
+var app = angular.module('tschedule', ['firebase'])
+
+app.controller('ForumCtrl', [
+    '$scope',
+    '$firebaseAuth',
+    '$window',
+    function($scope, $firebaseAuth, $window) {
+        var auth = $firebaseAuth();
+        $scope.user = null;
+		$scope.comments = [];
+
+        auth.$onAuthStateChanged(function(user) {
+          if (user) {
+              $scope.user = user;
+          }
+        });
+
+        $scope.signout = function() {
+            auth.$signOut().then(function() {
+                $window.location.href = "/";
+            }, function(error) {
+                alert("Error during sign out. Try again", error);
+            });
+        }
+
+		$scope.postComment = function() {
+			if ($scope.textComment == '') {
+				return;
+			}
+			$scope.comments.push($scope.textComment);
+
+		    $scope.textComment = '';
+		}
+    }
+]);
